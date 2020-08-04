5d2c3ec1ecf2e87e4a9e39f2593f4e2b0eea4549
Minjae Park <mp999@drexel.edu>
2020-08-03 20:18:09 -0400

    display items in main page

--- app.js
+++ app.js
@@ -67,5 +67,7 @@ const PORT = process.env.PORT || 5000;
 // start listening
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`
+  )
 );
--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,19 @@
 const express = require("express");
 const router = express.Router();
 const conEnLogin = require("connect-ensure-login");
-const passport = require("passport");
+const Item = require("../models/item");
 
 // index page
-router.get("/", (req, res) => {
-  // get user
-  user = req.user;
+router.get("/", async (req, res) => {
+  // get items
+  items = await Item.find().lean();
+  console.log(items);
+
   args = {};
-  if(user){
-    args.dispName = user.f_name;
+  if(req.user){
+    args.dispName = req.user.f_name;
   }
+  args.items = items;
 
   // render index template
   res.render("index", args);
@@ -26,10 +29,13 @@ router.get("/logout", (req, res)=>{
   res.redirect("/auth/logout");
 });
 
-router.get('/profile', conEnLogin.ensureLoggedIn(), (req,res)=>{
+router.get('/profile', conEnLogin.ensureLoggedIn(), async (req,res)=>{
   // get user
   user = req.user;
+
+  // fill args
   args = {};
+  
   if(user){
     args.username = user.username;
     args.f_name = user.f_name;
